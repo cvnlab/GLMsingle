df090730c301ad3fad7675a7fe3d134116261bda
jk619 <jan.kurzawski@gmail.com>
2021-05-24 23:15:34 -0400

    adding cmapsign2

--- matlab/utilities/cmapsign2.m
+++ matlab/utilities/cmapsign2.m
@@ -1,15 +1,15 @@
-function f = cmapsign4(n)
+function f = cmapsign2(n)
 
-% function f = cmapsign4(n)
+% function f = cmapsign2(n)
 %
 % <n> (optional) is desired number of entries
 %   default: 64
 %
-% return a cyan-blue-black-red-yellow colormap.
+% return a blue-white-red colormap.
 % suitable for ranges like [-X X].
 %
 % example:
-% figure; imagesc(rand(100,100)); axis equal tight; colormap(cmapsign4); colorbar;
+% figure; imagesc(randn(100,100)); axis equal tight; colormap(cmapsign2); colorbar;
 
 % inputs
 if ~exist('n','var') || isempty(n)
@@ -18,13 +18,9 @@ end
 
 % constants
 colors = [
-  .8 1 1 % cyan-white
-  0 1 1  % cyan
   0 0 1  % blue
-  0 0 0  % black
+  1 1 1  % white
   1 0 0  % red
-  1 1 0  % yellow
-  1 1 .8 % yellow-white
   ];
 
 % do it (MAYBE CONSOLIDATE THIS CODE?)
