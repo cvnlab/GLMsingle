82845815d690a0f8196d68c50bf1ef489cd1b8cb
Remi Gau <remi_gau@hotmail.com>
2022-04-07 18:08:36 +0200

    add doc in CI yml and scripts

--- .github/workflows/tests_matlab.m
+++ .github/workflows/tests_matlab.m
@@ -1,12 +1,11 @@
 root_dir = getenv('GITHUB_WORKSPACE');
 
+% MOxUnit and MOcov need to be in the matlab path
 addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
-
 cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
 run moxunit_set_path();
 
+% adds GLM single to the path and runs all the tests
 cd(fullfile(root_dir));
-
 setup();
-
 run run_tests();
