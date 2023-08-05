3a78dad1d3b939ccc47c0e4c061b5882eb1cf198
Ian Charest <charest.ian@gmail.com>
2023-08-05 00:19:37 -0400

    fix calcbadness using special case for zero divisions.

--- glmsingle/utils/zerodiv.py
+++ glmsingle/utils/zerodiv.py
@@ -1,59 +1,56 @@
 import numpy as np
+import warnings
 
-
-def zerodiv(data1, data2, val=0, wantcaution=1):
+def zerodiv(x, y, val=0, wantcaution=1):
     """zerodiv(data1,data2,val,wantcaution)
     Args:
-        <data1>,<data2> are matrices of the same size or either
+        <x>,<y> are matrices of the same size or either
                         or both can be scalars.
-        <val> (optional) is the value to use when <data2> is 0.
+        <val> (optional) is the value to use when <y> is 0.
                         default: 0.
         <wantcaution> (optional) is whether to perform special
                         handling of weird cases (see below).
                         default: 1.
-        calculate data1./data2 but use <val> when data2 is 0.
+        calculate x/y but use <val> when y is 0.
         if <wantcaution>, then if the absolute value of one or
-                        more elements of data2 is less than 1e-5
+                        more elements of y is less than 1e-5
                         (but not exactly 0), we issue a warning
                         and then treat these elements as if they
                         are exactly 0.
         if not <wantcaution>, then we do nothing special.
 
     note some weird cases:
-    if either data1 or data2 is [], we return [].
-    NaNs in data1 and data2 are handled in the usual way.
+    if either x or y is [], we return [].
 
     """
 
-    # handle special case of data2 being scalar
-    if np.isscalar(data2):
-        if data2 == 0:
-            f = np.tile(val, data1.shape)
+    # Check if either x or y is empty, return empty if so
+    if x.size == 0 or y.size == 0:
+        return np.array([])
+
+    # handle special case of y being scalar
+    if np.isscalar(y):
+        if y == 0:
+            return np.full(x.shape, val)
         else:
-            if wantcaution and abs(data2) < 1e-5:
-                print(
-                    'warning: abs value of divisor is less than 1e-5.'
-                    'treating the divisor as 0.')
-                f = np.tile(val, data1.shape)
+            if wantcaution and abs(y) < 1e-5:   # see allzero.m
+                warnings.warn('abs value of divisor is less than 1e-5. we are treating the divisor as 0.')
+                return np.full(x.shape, val)
             else:
-                f = data1/data2
-
+                return x / y[:, np.newaxis]
     else:
-        # do it
-        bad = data2 == 0
-        bad2 = abs(data2) < 1e-5
-        if wantcaution and np.any(np.logical_and(bad2.ravel(), np.logical_not(bad.ravel()))):
-            print(
-                'warning: abs value of one or more divisors'
-                'less than 1e-5.treating them as 0.')
-
+        bad = y == 0
+        bad2 = abs(y) < 1e-5  # see allzero.m
+        if wantcaution and np.any(np.logical_and(bad2, ~bad)):
+            warnings.warn('abs value of one or more divisors is less than 1e-5. we are treating these divisors as 0.')
         if wantcaution:
-            data2[bad2] = 1
-            f = data1/data2
+            tmp = y
+            tmp[bad2] = 1
+            f = x / tmp[:, np.newaxis]
             f[bad2] = val
         else:
-            data2[bad] = 1
-            f = data1/data2
+            tmp = y
+            tmp[bad] = 1
+            f = x / tmp[:, np.newaxis]
             f[bad] = val
-
-    return f
+        return f
