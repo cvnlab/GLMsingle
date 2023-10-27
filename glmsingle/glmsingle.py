std ddof fix discrepancy

glmsingle/glmsingle.py
@@ -1225,7 +1225,7 @@ class GLM_single():
                 noise_pool = noise_pool @ noise_pool.T
                 unitary = np.linalg.svd(noise_pool)[0]
                 unitary = unitary[:, :params['n_pcs']+1]
-                unitary = unitary / np.std(unitary, 0)
+                unitary = unitary / np.std(unitary, 0, ddof=1)
                 pcregressors.append(unitary.astype(np.float32))
 
 

glmsingle/ssq/calcbadness.py
@@ -78,7 +78,7 @@ def calcbadness(xvals, validcolumns, stimix, results, sessionindicator):
         mn = np.mean(results[0][:, whcol], axis=1)
 
         # std dev of unregularized case
-        sd = np.std(results[0][:, whcol], axis=1)
+        sd = np.std(results[0][:, whcol], axis=1, ddof=1)
 
         resultsdm = copy.deepcopy(results)
         for runis in range(len(resultsdm)):